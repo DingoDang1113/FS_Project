e825185c185a7afe6219724e9f65b1a0b9581cf6
DingoDang1113 <Shalpeng109@gmail.com>
2023-08-18 13:54:38 -0700

    configured for hosting

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,7 @@ module WeekDay
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.railties_order = [:all, :main_app]
     
     #enable and configure cookies
     config.middleware.use ActionDispatch::Cookies
